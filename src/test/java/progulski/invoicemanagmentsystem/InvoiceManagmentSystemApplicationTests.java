406fbc78ee4a1f783c9c32d0503327bb7a03ab6a
pawelrogulski <pawelrogulski@o2.pl>
2023-03-24 18:29:32 +0100

    initial commit

---
+++ src/test/java/progulski/invoicemanagmentsystem/InvoiceManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package progulski.invoicemanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
